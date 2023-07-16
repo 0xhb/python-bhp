078640de3e56e38a362eea6a74f2a62d7e58dacd
Bou.ELH <boustta.elh@gmail.com>
2023-07-16 17:36:27 +0100

    build the tcp client script

--- udp_client.py
+++ udp_client.py
@@ -1,3 +1,20 @@
 import socket
 
-client = socket.socket(socket.AF_INET)
+host = "www.google.com"
+port = 80
+
+print("step 1")
+# make a socket object over udp
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print("step 1")
+# send some data to the server
+client.sendto(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n", (host, port))
+
+print("step 1")
+# recieve server response
+data, addr = client.recvfrom(4096)
+
+print("step 1")
+print(data.decode())
+client.close()
